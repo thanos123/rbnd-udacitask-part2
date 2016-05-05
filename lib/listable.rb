39fadc33a55463c51727117280ba5dd276d193a7
Thanos Atskakanis <thanosatskakanis@gmail.com>
2016-05-05 11:13:28 +0100

    added formatting methods to Listable module

--- lib/listable.rb
+++ lib/listable.rb
@@ -2,4 +2,17 @@ module Listable
   def format_description(description)
     "#{description}".ljust(30)
   end
+  def format_priority(priority)
+    value = " ⇧" if priority == "high"
+    value = " ⇨" if priority == "medium"
+    value = " ⇩" if priority == "low"
+    value = "" if !priority
+    return value
+  end
+  def format_date(start_date, end_date = nil)
+    dates = start_date.strftime("%D") if start_date
+    dates << " -- " + end_date.strftime("%D") if end_date
+    dates = "N/A" if !dates
+    return dates
+  end
 end
